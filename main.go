b4064341a4d0d231f4d8e8bed6c0b9b3817cad5c
rizki <rizki_yoist@yahoo.com>
2024-11-20 18:36:18 +0700

    Fixed sorting by date

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"sort"
 	"strings"
 )
 
@@ -13,22 +14,26 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
+
 	os.Mkdir("cvrt", 0777)
+
+	failedCount := 0
 	for _, fileName := range fileNames {
 		noExtFileName := removeExtension(fileName)
 		cmd := exec.Command("magick", fileName, "-resize", "50%", "JPG:"+noExtFileName+"_resized.jpg")
-		out, err := cmd.CombinedOutput()
+		_, err := cmd.CombinedOutput()
 		if err != nil {
+			failedCount++
 			fmt.Println(fileName, err)
+			continue
 		}
-		fmt.Println(string(out))
 		cmd = exec.Command("mv", noExtFileName+"_resized.jpg", "cvrt/"+noExtFileName+"_resized.jpg")
 		_, err = cmd.CombinedOutput()
 		if err != nil {
-			fmt.Println(fileName, err)
+			fmt.Println("failed copying: "+fileName, err)
 		}
 	}
-	fmt.Println("done")
+	fmt.Printf("succesfully converted %d files, failed: %d", len(fileNames)-failedCount, failedCount)
 }
 
 func findFileNames() ([]string, error) {
@@ -36,6 +41,19 @@ func findFileNames() ([]string, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed reading files: %v", err)
 	}
+
+	sort.Slice(files, func(i, j int) bool {
+		t1, err := files[i].Info()
+		if err != nil {
+			fmt.Println(err)
+		}
+		t2, err := files[j].Info()
+		if err != nil {
+			fmt.Println(err)
+		}
+		return t1.ModTime().After(t2.ModTime())
+	})
+
 	fileNames := []string{}
 
 	for _, file := range files {
